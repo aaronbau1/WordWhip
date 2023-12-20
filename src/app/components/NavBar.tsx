50f2ec23afd1b6d265be3bde24a15a4aa50d6457
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-20 06:03:37 -0500

    added NavBar styling

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,6 +1,10 @@
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className="flex h-14 w-full border-2 border-black items-center justify-center bg-gray-700">
+      <div className="text-5xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-br from-red-700 to-yellow-400 top-0">
+        WordWhip
+      </div>
+    </div>
   )
 }
 
