53178160fd99a8993735c3054bd42cd42fc6743c
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-20 06:20:07 -0500

    Added InstructionBar styling

--- src/app/components/InstructionBar.tsx
+++ src/app/components/InstructionBar.tsx
@@ -1,6 +1,10 @@
 const InstructionBar = () => {
   return (
-    <div>InstructionBar</div>
+    <div className="flex flex-col items-center justify-center">
+      <div className='flex mt-5 h-12 w-9/12 border-2 rounded-full border-black items-center justify-center bg-white'>
+        Place a Tile!
+      </div>
+    </div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
-import NavBar from "./components/NavBar";
+"use client"
 
-export default function Home() {
+import { observer, useLocalObservable } from "mobx-react-lite";
+
+import GameGrid from "./components/GameGrid";
+import InstructionBar from "./components/InstructionBar";
+import NavBar from "./components/NavBar";
+import TilesBar from "./components/TilesBar";
+import GameStore from "../../stores/GameStore";
+
+export default observer(function Home() {
+  const store = useLocalObservable(() => GameStore)
   return (
-    <>
+    <div className="flex h-screen w-screen flex-col bg-gray-200">
       <NavBar />
-      <div>Home</div>
-    </>
+      <InstructionBar />
+      <GameGrid />
+      <TilesBar />
+    </div>
   )
-}
+})
