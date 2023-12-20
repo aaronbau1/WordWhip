c6a90ca0ca4cac8c3bca7a686f3df050a49ad844
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-20 07:11:50 -0500

    styled GameGrid and created GameTile component

--- src/app/components/GameGrid.tsx
+++ src/app/components/GameGrid.tsx
@@ -1,6 +1,30 @@
+import GameTile from "./GameTile"
+
 const GameGrid = () => {
   return (
-    <div>GameGrid</div>
+    <div className=" flex justify-center mt-5">
+      <div className="grid grid-cols-5 gap-2 border-2">
+        {new Array(5).fill(0).map((_, i) => {
+          
+          return (
+            <>
+              <GameTile row={i} col={0}/>
+              {/* <div key={`${i}`+ '0'} className="w-16 h-16 bg-white border-black-200">
+                {`0${i}`}
+              </div>
+              <div key={`${i}`+ '1'} className="w-16 h-16 bg-white border-black-200">
+              </div>
+              <div key={`${i}`+ '2'} className="w-16 h-16 bg-white border-black-200">
+              </div>
+              <div key={`${i}`+ '3'} className="w-16 h-16 bg-white border-black-200">
+              </div>
+              <div key={`${i}`+ '4'} className="w-16 h-16 bg-white border-black-200">
+              </div> */}
+            </>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
