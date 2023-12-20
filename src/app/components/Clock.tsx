72e5ca53f17105ede68efe554c0210bff587ef72
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-20 14:37:38 -0500

    Created clock component and integrated into UI

---
+++ src/app/components/Clock.tsx
@@ -0,0 +1,11 @@
+const Clock = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className='flex mt-5 h-12 w-1/4 border-2 rounded-full border-black items-center justify-center bg-white'>
+        1:00
+      </div>
+    </div>
+  )
+}
+
+export default Clock
