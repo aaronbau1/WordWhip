1dfb3faf78973a58c60622865309711a94f3d8d9
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2024-01-09 23:36:16 -0500

    Added DnD ux / ui for Tile bar component

--- src/app/components/InstructionBar.tsx
+++
@@ -1,9 +0,0 @@
-const InstructionBar = () => {
-  return (
-    <div className='flex mt-5 h-12 w-1/5 border-2 rounded-full border-black items-center justify-center bg-white'>
-      Score: 0
-    </div>
-  )
-}
-
-export default InstructionBar
--- src/app/components/TilesBar.tsx
+++ src/app/components/TilesBar.tsx
@@ -1,15 +1,37 @@
 "use client"
 
-// interface 
+import { useState } from "react";
 
-const TilesBar = ({handleOnDrag}) => {
+const TilesBar = () => {
+  const [isGrabbing, setIsGrabbing] = useState<boolean>(false);
+
+  const handleMouseDown = () => {
+    setIsGrabbing(true);
+    document.body.style.cursor = "grabbing";
+  };
+
+  const handleMouseUp = () => {
+    setIsGrabbing(false);
+    document.body.style.cursor = "default"; // Reset cursor when released
+  };
+
+  const handleOnDrag = (event: React.DragEvent<HTMLDivElement>, value: string) => {
+    event.dataTransfer.setData("text/plain", value);
+    if (event.currentTarget instanceof HTMLElement) {
+      event.currentTarget.style.opacity = "1";
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center pt-10">
-      <div className='flex h-14 w-14 border-2 border-black items-center justify-center bg-gray-300 text-2xl font-bold text-gray-700 cursor-pointer'
+      <div className='flex h-14 w-14 border-2 border-black/60 items-center justify-center bg-gray-300 text-2xl font-bold text-gray-700 cursor-pointer'
         draggable
-        onDragStart={(e) => handleOnDrag(e, 'A')}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onDragStart={(e) => handleOnDrag(e, 'B')}
+        style={{ opacity: 1 }}
       >
-        A
+        B
       </div>
     </div>
   )
