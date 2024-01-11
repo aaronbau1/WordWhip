b192744403322ea159d8c1139177d92cb4122956
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2024-01-10 23:00:20 -0500

    Implemented Trie data structure for searching solution algorithm

---
+++ lib/utils/Trie.ts
@@ -0,0 +1,48 @@
+import validWords from "../data";
+import answers from "../data";
+
+interface TrieNode {
+  children: { [key: string]: TrieNode };
+  isEndOfWord: boolean;
+}
+
+class Trie {
+  private root: TrieNode;
+
+  constructor() {
+    this.root = this.createTrieNode();
+  }
+
+  private createTrieNode(): TrieNode {
+    return { children: {}, isEndOfWord: false };
+  }
+
+  insert(word: string): void {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children[char]) {
+        node.children[char] = this.createTrieNode();
+      }
+      node = node.children[char];
+    }
+    node.isEndOfWord = true;
+  }
+
+  search(word: string): boolean {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children[char]) {
+        return false;
+      }
+      node = node.children[char];
+    }
+    return node.isEndOfWord;
+  }
+}
+
+//initialize Trie with valid words
+const trie = new Trie();
+validWords.forEach((word) => trie.insert(word));
+
+export default trie;
+
