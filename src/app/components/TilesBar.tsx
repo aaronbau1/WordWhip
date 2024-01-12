9167c36c2c0f50c121ebc14467c863bac936bb45
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2024-01-11 19:22:49 -0500

    Fixed hydration issue with SSR of initial random tile, converted to useEffect

--- src/app/components/TilesBar.tsx
+++ src/app/components/TilesBar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const TilesBar = () => {
 
@@ -11,25 +11,46 @@ const TilesBar = () => {
     return randomCapitalLetter;
   }
 
-  const [tileValue, setTileValue] = useState<string>(getRandomCapitalLetter())
+  const [tileValue, setTileValue] = useState<string | null>(null);
 
-  const handleOnDrag = (event: React.DragEvent<HTMLDivElement>, value: string) => {
-    event.dataTransfer.setData("text/plain", value);
-    if (event.currentTarget instanceof HTMLElement) {
-      event.currentTarget.style.opacity = "1";
+  useEffect(() => {
+    setTileValue(getRandomCapitalLetter());
+  }, []);
+
+  const handleOnDrag = (event: React.DragEvent<HTMLDivElement>, value: string | null) => {
+    if (value) {
+      event.dataTransfer.setData("text/plain", value);
+      if (event.currentTarget instanceof HTMLElement) {
+        event.currentTarget.style.opacity = "1";
+      }
     }
   };
 
-  const handleOnDrop = (event: React.DragEvent<HTMLDivElement>) => {
-    setTileValue(getRandomCapitalLetter());
+  const handleOnDragEnd = (event: React.DragEvent<HTMLDivElement>) => {
+    // const isDropOnValidCell = event.currentTarget.classList.contains('valid-cell-class');
+    
+    // setTileValue(getRandomCapitalLetter());
+    const isDropOnGameBoardCell = event.target instanceof HTMLElement && event.target.classList.contains('data-test');
+
+    // Update the tile value based on the validity of the drop
+    setTileValue(isDropOnGameBoardCell ? getRandomCapitalLetter() : tileValue);
   };
+
+  // const handleOnDrop = (event: React.DragEvent<HTMLDivElement>) => {
+  //   // Check if the drop occurred on a valid game cell
+  //   const isDropOnGameBoardCell = event.target instanceof HTMLElement && event.target.classList.contains('data-test');
+
+  //   // Update the tile value based on the validity of the drop
+  //   setTileValue(isDropOnGameBoardCell ? getRandomCapitalLetter() : tileValue);
+  // };
 
   return (
     <div className="flex flex-col items-center justify-center pt-10">
       <div className='flex h-14 w-14 border-2 border-black/60 items-center justify-center bg-gray-300 text-2xl font-bold text-gray-700 cursor-pointer'
         draggable
         onDragStart={(e) => handleOnDrag(e, tileValue)}
-        onDragEnd={handleOnDrop}
+        // onDrop={handleOnDrop}
+        onDragEnd={handleOnDragEnd}
         style={{ opacity: 1 }}
       >
         {tileValue}
