ed6eb2cb580f468054ee2b8502249b5a263696fe
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2024-01-11 22:10:16 -0500

    Added multiple tiles to bar and implemented TileBar state

--- src/app/components/TilesBar.tsx
+++ src/app/components/TilesBar.tsx
@@ -1,23 +1,21 @@
 "use client"
 
 import { useEffect, useState } from "react";
+import { getRandomCapitalLetter } from "../../../lib/hooks";
+
+// interface TileBarProps {
+//   updateTileBar: (newTileValue: string) => void;
+// }
 
 const TilesBar = () => {
 
-  const getRandomCapitalLetter = ():string => {
-    // ASCII codes for capital letters range from 65 to 90
-    const randomCharCode = Math.floor(Math.random() * (90 - 65 + 1)) + 65;
-    const randomCapitalLetter = String.fromCharCode(randomCharCode);
-    return randomCapitalLetter;
-  }
-
-  const [tileValue, setTileValue] = useState<string | null>(null);
+  const [tileValues, setTileValues] = useState<string[]>([]);
 
   useEffect(() => {
-    setTileValue(getRandomCapitalLetter());
+    setTileValues([getRandomCapitalLetter(), getRandomCapitalLetter()]);
   }, []);
 
-  const handleOnDrag = (event: React.DragEvent<HTMLDivElement>, value: string | null) => {
+  const handleOnDrag = (event: React.DragEvent<HTMLDivElement>, value: string | null, index: number) => {
     if (value) {
       event.dataTransfer.setData("text/plain", value);
       if (event.currentTarget instanceof HTMLElement) {
@@ -26,15 +24,10 @@ const TilesBar = () => {
     }
   };
 
-  const handleOnDragEnd = (event: React.DragEvent<HTMLDivElement>) => {
-    // const isDropOnValidCell = event.currentTarget.classList.contains('valid-cell-class');
-    
-    // setTileValue(getRandomCapitalLetter());
-    const isDropOnGameBoardCell = event.target instanceof HTMLElement && event.target.classList.contains('data-test');
-
-    // Update the tile value based on the validity of the drop
-    setTileValue(isDropOnGameBoardCell ? getRandomCapitalLetter() : tileValue);
-  };
+  // const handleOnDragEnd = (event: React.DragEvent<HTMLDivElement>) => {
+  //   // setTileValue(isDropOnGameBoardCell ? getRandomCapitalLetter() : tileValue);
+  //   // updateTileBar(newTileValue);
+  // };
 
   // const handleOnDrop = (event: React.DragEvent<HTMLDivElement>) => {
   //   // Check if the drop occurred on a valid game cell
@@ -45,16 +38,20 @@ const TilesBar = () => {
   // };
 
   return (
-    <div className="flex flex-col items-center justify-center pt-10">
-      <div className='flex h-14 w-14 border-2 border-black/60 items-center justify-center bg-gray-300 text-2xl font-bold text-gray-700 cursor-pointer'
-        draggable
-        onDragStart={(e) => handleOnDrag(e, tileValue)}
-        // onDrop={handleOnDrop}
-        onDragEnd={handleOnDragEnd}
-        style={{ opacity: 1 }}
-      >
-        {tileValue}
-      </div>
+    <div className="flex items-center justify-center pt-8 pb-4">
+      { 
+        tileValues.map((value, index) => (
+          <div
+            className='flex h-14 w-14 border-2 border-black/60 items-center 
+            justify-center bg-gray-300 text-2xl font-bold text-gray-700 
+            cursor-pointer m-1'
+            draggable
+            onDragStart={(e) => handleOnDrag(e, value, index)}
+          >
+            {value}
+          </div>
+        ))      
+      }
     </div>
   )
 }
