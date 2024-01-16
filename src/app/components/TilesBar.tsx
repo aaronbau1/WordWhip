07ed3b568aa54c855c2a527a88a3e954cbd8a6e6
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2024-01-15 22:38:00 -0500

    Made tiles match GameBoardCell aspect ratio

--- src/app/components/TilesBar.tsx
+++ src/app/components/TilesBar.tsx
@@ -18,28 +18,17 @@ const TilesBar = ({ tileValues }: TilesBarProps) => {
     }
   };
 
-  // const handleOnDragEnd = (event: React.DragEvent<HTMLDivElement>) => {
-  //   // setTileValue(isDropOnGameBoardCell ? getRandomCapitalLetter() : tileValue);
-  //   // updateTileBar(newTileValue);
-  // };
-
-  // const handleOnDrop = (event: React.DragEvent<HTMLDivElement>) => {
-  //   // Check if the drop occurred on a valid game cell
-  //   const isDropOnGameBoardCell = event.target instanceof HTMLElement && event.target.classList.contains('data-test');
-
-  //   // Update the tile value based on the validity of the drop
-  //   setTileValue(isDropOnGameBoardCell ? getRandomCapitalLetter() : tileValue);
-  // };
-
   return (
     <div className="flex items-center justify-center pt-6 pb-4">
       { 
         tileValues.map((value, index) => (
           <div
           key={index}
-            className='flex h-14 w-14 border-2 border-black/60 items-center 
-            justify-center bg-gray-300 text-2xl font-bold text-gray-700 
-            cursor-pointer m-1'
+            // className='flex h-14 w-14 border-2 border-black/60 items-center 
+            // justify-center bg-gray-300 text-2xl font-bold text-gray-700 
+            // cursor-pointer m-1'
+            className="flex h-[10vw] w-[10vw] max-w-screen-md max-h-screen-md border-2 border-black/60 items-center justify-center bg-gray-300 text-2xl font-bold text-gray-700 cursor-pointer m-1"
+
             draggable
             onDragStart={(e) => handleOnDrag(e, value, index)}
           >
