12faaa428d70011ab7e7a09e3fecdeda1c4c5ba7
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2024-01-15 20:34:31 -0500

    Implemented puzzle building algorithm and game logic, need to clean up

--- src/app/components/GameBoard.tsx
+++ src/app/components/GameBoard.tsx
@@ -3,37 +3,70 @@ import GameBoardCell from "./GameBoardCell"
 import trie from "../../../lib/utils/Trie";
 import { gameBoardLines } from "../../../lib/data";
 import TilesBar from "./TilesBar";
-import { getRandomCapitalLetter } from "../../../lib/hooks";
+import { createSolutionLine, getRandomCapitalLetter, getRandomIndex, getRandomLine, getRandomWord } from "../../../lib/hooks";
 
 const GameBoard = () => {
 
   const [highlightedCell, setHighlightedCell] = useState<number | null>(null);
-  const [boardValues, setBoardValues] = useState<string[]>(Array(25).fill('A'));
+  const [boardValues, setBoardValues] = useState<string[]>(Array(25).fill(''));
   const [tileValues, setTileValues] = useState<string[]>([]);
 
-
+  //initialize the puzzle
   //BUG FOUND: IF BOTH ARE SAME LETTER, WILL UPDATE WITH ONLY ONE TILE IF PLACED
   useEffect(() => {
-    setTileValues([getRandomCapitalLetter(), getRandomCapitalLetter()]);
+    const solution = getRandomWord();
+    let solutionLine = getRandomLine();
+    const randomIndex = getRandomIndex();
+    let randomValues
+    // initializeBoardValues(solution, solutionLine, randomIndex);
+    do {
+      randomValues = boardValues.map((value, index) => {
+        if (solutionLine.includes(index) && index !== solutionLine[randomIndex]) {
+          return solution[solutionLine.indexOf(index)].toUpperCase();
+        } else {
+          return getRandomCapitalLetter();
+        }
+      });
+    } while (checkBoard(gameBoardLines, randomValues).length !== 0)
+  
+    setBoardValues(randomValues);
+    setTileValues([solution[randomIndex].toUpperCase()]);
   }, []);
-
 
   // Check for matches after the boardValues state has been updated
   useEffect(() => {
-    const wordsArray = linesToWords(gameBoardLines);
-    const findMatch = wordsArray.filter((word) => trie.search(word));
-    if (findMatch[0]) alert(`You found the word: ${findMatch[0]}`);
-
+    const matchingWords = checkBoard(gameBoardLines, boardValues);
+    if (matchingWords.length > 0) {
+      alert(`You found the word: ${matchingWords.join(", ")}`);
+    }
   }, [boardValues]);
 
-  const handleDragLeave = () => {
-    setHighlightedCell(null);
+  const checkBoard = (gameBoardLines: number[][], valueArray:string[]): string[] => {
+    const wordsArray = linesToWords(gameBoardLines, valueArray);
+    return wordsArray.filter((word) => trie.search(word));
   };
 
-  const linesToWords = (gameBoardLines: number[][]):string[] => {
+  const initializeBoardValues = (solution:any, solutionLine:any, randomIndex:any) => {
+    solutionLine.splice(randomIndex,1)
+    let randomValues;
+    
+    do {
+      randomValues = boardValues.map((value, index) => {
+        if (solutionLine.includes(index)) {
+          return solution[solutionLine.indexOf(index)].toUpperCase();
+        } else {
+          return getRandomCapitalLetter();
+        }
+      })
+    } while (checkBoard(gameBoardLines, randomValues).length !== 0)
+
+    setBoardValues(randomValues);
+  };
+
+  const linesToWords = (gameBoardLines: number[][], valueArray:string[]):string[] => {
     return gameBoardLines.map((line) => {
       // GameBoardCell values to word
-      return line.map((id) => (boardValues[id]).toLowerCase()).join('');
+      return line.map((value) => (valueArray[value]).toLowerCase()).join('');
     })
   }
 
@@ -47,6 +80,10 @@ const GameBoard = () => {
     updatedValues[id] = value;
     setBoardValues(updatedValues);
     updateTileBar(value);
+  };
+
+  const handleDragLeave = () => {
+    setHighlightedCell(null);
   };
 
   return (
