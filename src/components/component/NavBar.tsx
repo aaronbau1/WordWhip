550ee81f23f315dd03c734fb595d31c85e5c5a18
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2024-01-23 22:12:25 -0500

    Reworked mobile and desktop styling for NavBar

--- src/components/component/NavBar.tsx
+++ src/components/component/NavBar.tsx
@@ -12,14 +12,10 @@ const NavBar = () => {
 
   const {isInfoModalOpen, openInfoModal} = useInfoModal();
 
-  // useEffect(() => {
-  //   isInfoModalOpen ? openInfoModal() : closeInfoModal();
-  // }, [isInfoModalOpen]);
-
   return (
-    <div className="flex h-14 w-full border-2 border-black items-center justify-between bg-gray-700">
+    <div className="flex h-16 md:h-[4.25rem] w-full border-2 border-black items-center justify-between bg-gray-700">
       <SettingsModal />
-      <div className="text-5xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-br from-red-700 to-yellow-400 top-0">
+      <div className="text-4xl md:text-5xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-br from-red-700 to-yellow-400 top-0">
         <Link href='/'>WordWhip</Link>
       </div>
       <InfoModal />
