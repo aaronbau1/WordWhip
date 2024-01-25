9fed02079d08cdf4134616e31260567846088afa
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2024-01-25 11:21:51 -0500

    Cleaned up leftovers from redux migration affecting deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { InfoModalProvider } from '@/context/InfoModalContext'
-import { InfoModal } from '@/components/component/InfoModal'
 import ReduxProvider from '@/lib/provider'
 
 const inter = Inter({ subsets: ['latin'] })
